c4751aa39e8662037056bed6f80c74db7a3f7b14
laoooba <yama10taku31@gmail.com>
2021-09-07 10:08:17 +0000

    add new.session.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,27 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+  
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+
+  def after_sign_in_path_for(resource)
+    if user_signed_in?
+      root_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :user
+      root_path
+    else
+      new_admin_session_path
+    end
+  end
+  
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(email: "jjj@gmail.com",
+             password:  "testjjj",
+             password_confirmation: "testjjj",
+             )
