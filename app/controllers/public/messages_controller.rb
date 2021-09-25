e1e062217e68f5d939096f9376d661c584d6f407
laoooba <yama10taku31@gmail.com>
2021-09-25 11:25:46 +0000

    add layout

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -4,8 +4,6 @@ class Public::MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-    else
-      flash[:alert] = "メッセージ送信に失敗しました。"
     end
     redirect_to room_path(@message.room_id)
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   #---------- アソシエーション---------
   belongs_to :user
   belongs_to :post
+  
+  validates :comment, presence: true
 
   #----------通知機能---------
   has_many :notifications, dependent: :destroy
